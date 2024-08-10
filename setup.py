0c01d7482908d0d3799d9d7c2889eb7c2116acb4
nigiledwin <nigilke9416@gmail.com>
2024-08-10 13:04:48 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Trying to create a machine learning model using MLOPS learnts from Nithish',
+    author='Nigil',
+    license='',
+)
